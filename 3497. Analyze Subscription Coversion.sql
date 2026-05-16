3c578b542f8498b81b9df57f11f0a68b231c09c9
junaidazhar42 <68844918+junaidazhar42@users.noreply.github.com>
2026-05-16 13:37:35 +1000

    Add files via upload

---
+++ 3497. Analyze Subscription Coversion.sql
@@ -0,0 +1,7 @@
+# 3497. Analyze Subscription Coversion
+
+SELECT user_id, ROUND(AVG(CASE WHEN activity_type = 'free_trial' THEN activity_duration ELSE NULL END), 2) AS trial_avg_duration, ROUND(AVG(CASE WHEN activity_type = 'paid' THEN activity_duration ELSE NULL END), 2) AS paid_avg_duration 
+FROM UserActivity
+GROUP BY user_id
+HAVING AVG(CASE WHEN activity_type = 'paid' THEN activity_duration ELSE NULL END) IS NOT NULL 
+ORDER BY user_id;
